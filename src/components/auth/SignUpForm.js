8e9f7442fb0c2b7c6dd350b1eceb7531e33f6984
kalachev_va <kalachev_va@ursip.ru>
2018-10-22 20:23:32 +0300

    add redux-form and validate"

---
+++ src/components/auth/SignUpForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import emailValidator from "email-validator";
+import ErrorField from "./ErrorField";
+
+class SignUpForm extends Component {
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <div>
+        <h2>Sign In</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email</label>
+            <Field name="email" component={ErrorField} />
+          </div>
+          <div>
+            <label>Password</label>
+            <Field name="password" component={ErrorField} type="password" />
+          </div>
+          <div>
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const validate = ({ email, password }) => {
+  const errors = {};
+
+  if (!email) errors.email = "email is required";
+  else if (!emailValidator.validate(email)) errors.email = "invalid email";
+
+  if (!password) errors.password = "passord is required";
+  else if (password.lenght < 8) errors.password = "to short";
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "auth",
+  validate
+})(SignUpForm);
