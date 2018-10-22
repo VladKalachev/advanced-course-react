5649f4b8b260c02f3e63e814a796dfce835a87f8
kalachev_va <kalachev_va@ursip.ru>
2018-10-22 18:59:30 +0300

    add route component

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import AdminPage from "./routes/AdminPage";
+import AuthPage from "./routes/AuthPage";
 
 class Root extends Component {
   render() {
-    return <p>Hello</p>;
+    return (
+      <div>
+        <Route path="/admin" component={AdminPage} />
+        <Route path="/auth" component={AuthPage} />
+      </div>
+    );
   }
 }
 
